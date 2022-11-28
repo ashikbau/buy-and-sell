d7e4a5fb65e3c9e38b3d8610e55d9f5104a8490c
Your Name <you@example.com>
2022-11-28 01:20:01 +0100

    payment partly added

--- src/Pages/Dashboard/Payment/CheckOutForm.js
+++ src/Pages/Dashboard/Payment/CheckOutForm.js
@@ -1,42 +1,71 @@
-// import React from 'react';
-// import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useState } from 'react';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 
-// const CheckOutForm = () => {
+const CheckOutForm = ({booking}) => {
+    const {price} = booking;
+    const [cardError,setCardError]= useState('')
 
-//     const stripe = useStripe();
-//     const elements = useElements();
+    const stripe = useStripe();
+    const elements = useElements();
 
-//     const handleSubmit = async (event) => {
-//         event.preventDefault();
-//         if (!stripe || !elements) {
-//             return
-//         }
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!stripe || !elements) {
+            return
+        }
+        const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+        type: 'card',
+        card,
+      });
+
+      if (error) {
+        console.log(error);
+        setCardError(error.message);
+    }
+    else {
+        setCardError('');
+    }
+  
 
 
         
 
-//     }
-//     return (
-//         <form onSubmit={handleSubmit}>
-//         <CardElement
-//             options={{
-//                 style: {
-//                     base: {
-//                         fontSize: '16px',
-//                         color: '#424770',
-//                         '::placeholder': {
-//                             color: '#aab7c4',
-//                         },
-//                     },
-//                     invalid: {
-//                         color: '#9e2146',
-//                     },
-//                 },
-//             }}
-//         />
-       
-//     </form>
-//     );
-// };
+    }
+    return (
+      <>
+        <form onSubmit={handleSubmit}>
+        <CardElement
+            options={{
+                style: {
+                    base: {
+                        fontSize: '16px',
+                        color: '#424770',
+                        '::placeholder': {
+                            color: '#aab7c4',
+                        },
+                    },
+                    invalid: {
+                        color: '#9e2146',
+                    },
+                },
+            }}
+        />
+        <button className='btn btn-sm btn-primary mt-4' type="submit" disabled={!stripe}>
+          Pay
+        </button>
+    </form>
+    <p className="text-red-500">{cardError}</p>
+      </>
+    
+    
+    );
+};
 
-// export default CheckOutForm;
+export default CheckOutForm;
--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,19 +1,27 @@
-// import { Elements } from '@stripe/react-stripe-js';
-// import { loadStripe } from '@stripe/stripe-js';
-// import React from 'react';
-// import { useLoaderData } from 'react-router-dom';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import CheckOutForm from './CheckOutForm';
 
 
-// const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 
-// const Payment = () => {
-//     const booking= useLoaderData();
-//     console.log(booking)
-//     return (
-//         <div>
-//             <h3> This is payment</h3>
-//         </div>
-//     );
-// };
+const Payment = () => {
+    const booking= useLoaderData();
+    console.log(booking)
+    return (
+        <div>
+            <h3>Please Pay for Car Price:$ {booking.price}</h3>
+            <div className='w-96 my-12'>
+            <Elements stripe={stripePromise}>
+      <CheckOutForm
+      booking={booking}
+      />
+    </Elements>
+            </div>
+        </div>
+    );
+};
 
-// export default Payment;
+export default Payment;
